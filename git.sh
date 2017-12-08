c2a5aba3fe9619092b0c368ab28f1bdb2569c5aa
mucahiddayan <muecahid@dayan.one>
2017-12-08 09:55:09 +0100

    dev 2017.12.08-09.55

--- git.sh
+++ git.sh
@@ -1,5 +1,6 @@
-CURRENT_TIME=date +"%Y.%m.%d-%H.%M"
+CURRENT_TIME=$(date +"%Y.%m.%d-%H.%M")
+PARENDIR="$(dirname "$PWD")"
 git add *
 git commit -m "dev $CURRENT_TIME"
 git push origin dev
-sudo cp * /var/www/html/wordpress/wp-content/plugins/$PWD
+sudo cp PARENDIR/$PWD /var/www/html/wordpress/wp-content/plugins/
