f4d6df5f143b2119706e0427af675b2248f691d7
mucahiddayan <muecahid@dayan.one>
2017-12-11 11:39:04 +0100

    dev 2017.12.11-11.39

--- dev_test/server.js
+++ dev_test/server.js
@@ -1,6 +1,6 @@
 var jsonServer = require('json-server')
 var server = jsonServer.create()
-var router = jsonServer.router('db.json')
+var router = jsonServer.router('./db.json')
 var middlewares = jsonServer.defaults()
 
 function simpleAuth(req, res, next) {
